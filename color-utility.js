3928b32e34604bf4ede481809d8316c9634d76ff
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-04-01 11:19:25 +0200

    Update color-utility.js

--- color-utility.js
+++ color-utility.js
@@ -1,3 +1,11 @@
 export function sayHello() {
     console.log('Hello, color utility package works!');
 }
+
+// 1 - setOpacity(hexColor, opacity) -> return hexColor con applicata opacity
+
+
+// 2 - fromColorStringToRGB(stringColor - deve essere hex. ottimale, deve essere il nome del colore, per esempio "red") -> return colore rgb
+
+
+// 3 - getContrastingColor(colorString) -> return string
